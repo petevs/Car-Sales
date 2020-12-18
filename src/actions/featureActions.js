16bb71c15343cfbab4f559589fd50510f9a0308c
Pete VS <pete_vs88@me.com>
2020-12-18 12:38:57 -0500

    connect header to redux

--- src/actions/featureActions.js
+++ src/actions/featureActions.js
@@ -1,3 +1,7 @@
 export const removeFeature = (featureId) => {
     return { type: 'REMOVE_FEATURE', payload: featureId}
 }
+
+export const addFeature = (featureId) => {
+    return { type: 'ADD_FEATURE', payload: featureId}
+}
